919b393df70f8d0c5e1b4cac7207ffe2383c7722
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-30 16:53:29 +0700

    Improve

--- System/ActionHandler.php
+++ System/ActionHandler.php
@@ -3,8 +3,11 @@
 namespace System;
 
 use System\Facebook;
+
 /**
 * @author	Ammar Faizi	<ammarfaizi2@gmail.com>
+* Script iki digawe angel banget :v
+* Jangan lupa bahagia kawanku :v
 */
 
 class ActionHandler
@@ -12,10 +15,12 @@ class ActionHandler
 	private $fb;
 	private $hash;
 	private $userdata;
-	private $friend_sugesstion_url;
+	private $action_add_friend = array();
+	private $friend_sugesstion_url = array();
 
 	public function __construct($email, $pass, $user=null)
 	{
+		date_default_timezone_set("Asia/Jakarta");
 		$this->hash = fb_data . '/' . md5($user.$pass) . '.txt';
 		$this->fb = new Facebook($email, $pass, $user);
 		$this->userdata = data . '/' . $this->hash . '_data.txt';
@@ -52,7 +57,8 @@ class ActionHandler
 
 	private function get_friend_sugesstion_url()
 	{
-		$a = file_get_contents('a.tmp');
+		# $a = file_get_contents('a.tmp');
+		$a = $this->fb->get_page("https://m.facebook.com/friends/center/suggestions");
 		$a = explode("/a/mobile/friends/add_friend.php", $a);
 		$this->friend_sugesstion_url = array();
 		for ($i=1; $i < count($a); $i++) { 
@@ -60,6 +66,13 @@ class ActionHandler
 			preg_match("#id=(.*)&amp;#", $b[0], $n);
 			$n = explode("&", $n[1]);
 			$this->friend_sugesstion_url[$n[0]] = "https://m.facebook.com/a/mobile/friends/add_friend.php".html_entity_decode($b[0], ENT_QUOTES, 'UTF-8');
+		}
+	}
+
+	private function do_add_friend()
+	{
+		foreach ($this->friend_sugesstion_url as $key => $value) {
+			
 		}
 	}
 }
