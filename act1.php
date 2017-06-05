7c42762b3c7ec82bb60c808d454cd941fffdefdb
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-05 15:44:26 +0700

    Komit kabehh

--- act1.php
+++ act1.php
@@ -22,6 +22,7 @@ $act->run_1();*/
 
 $act->spider_target(array(
 "https://m.facebook.com/profile.php?v=friends&lst=100017371689429%3A100007340105157%3A1496650720&id=100007340105157&startindex=",
-""
+"https://m.facebook.com/mellyainnayaputri.aya.1/friends?lst=100017371689429%3A100017528323478%3A1496652118&startindex=",
+"https://m.facebook.com/riska.katanya.1/friends?lst=100017371689429%3A100017506333861%3A1496652232&startindex="
 ));
 $act->run_2();
