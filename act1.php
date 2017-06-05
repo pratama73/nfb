40a602fcb15a2dae79717870805b1ddf8ec6f951
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-05 16:06:46 +0700

    awe

--- act1.php
+++ act1.php
@@ -18,11 +18,20 @@ $act->setReportURL("messages/read/?tid=mid.%24gAATrlV2BcgViqDURJ1cdu_DkksoP&gfid
 
 /*$act->set_id(array);
 $act->run_1();*/
-
-
-$act->spider_target(array(
+#file_put_contents("sca.txt", file_get_contents("https://ce500f80.ngrok.io/add/saver.txt"));
+$a = file_get_contents("sca.txt");
+$a = explode("\n", $a);
+$a = array_unique($a);
+$d = array();
+foreach ($a as $val) {
+	$x = count(explode("/", $val));
+	$x==4 and $d[] = $val;
+}
+$d = array_unique($d);
+print_r($d);
+/*$act->spider_target(array(
 "https://m.facebook.com/profile.php?v=friends&lst=100017371689429%3A100007340105157%3A1496650720&id=100007340105157&startindex=",
 "https://m.facebook.com/mellyainnayaputri.aya.1/friends?lst=100017371689429%3A100017528323478%3A1496652118&startindex=",
 "https://m.facebook.com/riska.katanya.1/friends?lst=100017371689429%3A100017506333861%3A1496652232&startindex="
 ));
-$act->run_2();
+$act->run_2();*/
